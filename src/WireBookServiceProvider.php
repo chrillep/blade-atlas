8a1f81491d0b34a578efc1df626bc989972802c5
Christian Widlund <christian.widlund@gmail.com>
2025-09-07 14:31:36 +0200

    🚚 (routes): Remove unused route definitions from service provider
    Eliminate unnecessary route macros from the WireBookServiceProvider. This cleanup helps streamline the codebase and improves maintainability by removing outdated routing logic.

--- src/WireBookServiceProvider.php
+++ src/WireBookServiceProvider.php
@@ -4,11 +4,9 @@ namespace Arrgh11\WireBook;
 
 use Arrgh11\WireBook\Commands\Stories;
 use Arrgh11\WireBook\Commands\Tools;
-use Arrgh11\WireBook\Controllers\StoryController;
 use Arrgh11\WireBook\Livewire\Application;
 use Arrgh11\WireBook\Livewire\Tests;
 use Arrgh11\WireBook\Tools\Viewport;
-use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -38,17 +36,6 @@ class WireBookServiceProvider extends PackageServiceProvider
     {
         Livewire::component('wirebook-app', Application::class);
         Livewire::component('wirebook-test-story', Tests\Button::class);
-
-        Route::macro('wirebook', function () {
-            Route::prefix('wirebook')->name('wirebook.')->group(function () {
-                Route::get('/dashboard', function () {
-                    return view('wirebook::application.index');
-                })->name('dashboard');
-
-                Route::get('/stories/{story}', [StoryController::class, 'index'])->name('story');
-
-            });
-        });
 
         \Arrgh11\WireBook\Facades\WireBook::registerTool(Viewport::class);
 
