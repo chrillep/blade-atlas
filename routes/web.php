401421c6ad397fa54526602640b1f98244b973db
Christian Widlund <christian.widlund@gmail.com>
2025-09-07 14:46:57 +0200

    🚚 (routes): Simplify WireBook route registration
    Refactor the route definitions for WireBook by invoking the macro defined in the service provider. This change streamlines the routing process and enhances maintainability by reducing the amount of code needed for route registration.

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,7 @@
 
 declare(strict_types=1);
 
-use Arrgh11\WireBook\Controllers\StoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('wirebook')->name('wirebook.')->group(function () {
-    Route::get('/', function () {
-        return redirect()->route('wirebook.dashboard');
-    })->name('root');
-
-    Route::get('/dashboard', function () {
-        return view('wirebook::application.index');
-    })->name('dashboard');
-
-    Route::get('/stories/{story}', [StoryController::class, 'index'])->name('story');
-
-});
+// Invoke the macro defined in the service provider to register WireBook routes.
+Route::wirebook();
